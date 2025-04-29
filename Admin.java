30ee419bc0459b635ed97de2971c06c88d7bafe3
Dewaeldri911 <campionaldri@gmail.com>
2025-04-29 23:40:38 +0800

    FileAdminPage
    file ini merupakan sebuah kode program mengenai tentang admin

---
+++ Admin.java
@@ -0,0 +1,21 @@
+package adminPage;
+
+public class Admin {
+    protected String username;
+    protected String password;
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
+
